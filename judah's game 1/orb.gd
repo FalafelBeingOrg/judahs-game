c82271aa84fdaf0839be209d19da07f5de6f27b9
Falafel <tadgranite@gmail.com>
2023-04-15 20:21:31 -0500

    made not touch walls

--- judah's game 1/orb.gd
+++ judah's game 1/orb.gd
@@ -1,13 +1,19 @@
 extends Area2D
-
-
+@onready var collider = $collider
+@onready var tilemap = get_node("/root/Node2D/TileMap")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = Vector2(randi_range(0,1151),randi_range(0, 647))
+	teleport()
 	print("hello")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if body_entered:
+		if overlaps_body(tilemap):
+			teleport()
+		
+func teleport():
+	position = Vector2(randi_range(0,1151),randi_range(0, 647))
+
