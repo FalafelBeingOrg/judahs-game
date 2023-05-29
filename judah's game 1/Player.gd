a7ac8e6385ad310e4b7f8865bd09c75f724ab886
Falafel <tadgranite@gmail.com>
2023-05-29 11:27:32 -0500

    a

--- judah's game 1/Player.gd
+++ judah's game 1/Player.gd
@@ -9,7 +9,7 @@ var move = Vector2(0,0)
 var lightDecay = 0.0006
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print(self.get_path())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
